83968ae6468e4082c3e192237f67491e76c03575
Gaoyang Zhang <gy@blurgy.xyz>
2020-11-24 22:29:01 +0800

    Add partial inplementation of function Zbuf::init_mvp()
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- include/Zbuf.cpp
+++ include/Zbuf.cpp
@@ -1,12 +1,41 @@
 #include "Zbuf.hpp"
+#include "glm/gtc/type_ptr.hpp"
 
-Zbuf::Zbuf() {}
-Zbuf::Zbuf(Scene const &s) : scene(s) {}
+Zbuf::Zbuf() { init(); }
+Zbuf::Zbuf(Scene const &s) : scene(s) { init(); }
+void Zbuf::init() { eye_initialized = false; }
 
-void Zbuf::set_eye(glm::vec3) {}
-void Zbuf::set_model(glm::mat3) {}
-void Zbuf::set_view(glm::mat3) {}
-void Zbuf::set_projection(glm::mat3) {}
+void Zbuf::set_cam(glm::vec3 const &ey, glm::vec3 const &g,
+                   glm::vec3 const &u) {
+    eye             = ey;
+    gaze            = g;
+    up              = u;
+    eye_initialized = true;
+}
+
+void Zbuf::init_mvp(glm::mat4 const &_model) {
+    if (!eye_initialized) {
+        errorm("Camera position is not initilized\n");
+    }
+    // Set model matrix
+    model = _model;
+
+    /**/
+    glm::vec3 right = glm::cross(gaze, up);
+    // clang-format off
+    double view_value[] = {
+        right.x, right.y, right.z, 0,
+           up.x,    up.y,    up.z, 0,
+        -gaze.x, -gaze.y, -gaze.z, 0,
+              0,       0,       0, 1,
+    };
+    // clang-format on
+    // Set view matrix, according to camera information
+    view = glm::make_mat4(view_value);
+
+    // Set projection matrix, according to fov, aspect ratio, etc.
+    // projection =
+}
 
 void Zbuf::naive() {}
 
--- include/Zbuf.hpp
+++ include/Zbuf.hpp
@@ -10,22 +10,30 @@ class Zbuf {
   private:
     Scene scene; // Scene to be rendered
 
-    glm::vec3 eye;   // Camera position
+    glm::vec3 eye;  // Camera position
+    glm::vec3 gaze; // Camera's gaze direction
+    glm::vec3 up;   // Camera's top direction
+    bool      eye_initialized;
+
     glm::mat4 model; // Transform the model to the next timestep
     glm::mat4 view;  // Transform the camera to origin, model moves with it
     glm::mat4 projection; // M_{persp} @ M_{ortho}, where M_{ortho}
                           // "compresses" the scene into the canonical box;
                           // M_{persp} performs perspective projection.
 
+    glm::mat4 mvp; // mvp = projection @ view @ model,
+                   // where '@' stands for matrix production
   public:
     Zbuf();
     // Initialize a zbuffer object with given scene
     Zbuf(Scene const &s);
+    // Set default values
+    void init();
 
-    void set_eye(glm::vec3);
-    void set_model(glm::mat3);
-    void set_view(glm::mat3);
-    void set_projection(glm::mat3);
+    void set_cam(glm::vec3 const &ey, glm::vec3 const &g, glm::vec3 const &u);
+
+    // Set mvp transformation matrix
+    void init_mvp(glm::mat4 const &_model = glm::mat4(0));
 
     void naive(); // Naive z-buffer
 };
