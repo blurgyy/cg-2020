bd9d1ec4a09ad56f75270fcf87814dff22b3b933
Gaoyang Zhang <gy@blurgy.xyz>
2020-11-24 12:26:52 +0800

    Add zbuf.{c,h}pp
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

---
+++ include/zbuf.cpp
@@ -0,0 +1,8 @@
+#include "zbuf.hpp"
+
+#include <cstdio>
+
+void zbuf::naive() {}
+
+// Author: Blurgy <gy@blurgy.xyz>
+// Date:   Nov 24 2020, 12:15 [CST]
---
+++ include/zbuf.hpp
@@ -0,0 +1,14 @@
+#ifndef ZBUF_HPP
+#define ZBUF_HPP
+
+namespace zbuf {
+
+// naive z-buffer implementation
+void naive();
+
+}; // namespace zbuf
+
+#endif
+
+// Author: Blurgy <gy@blurgy.xyz>
+// Date:   Nov 24 2020, 12:14 [CST]
