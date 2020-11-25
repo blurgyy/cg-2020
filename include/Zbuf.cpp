9b4ae5690b9eb792dfcd64afd1d56d5d13a01760
Gaoyang Zhang <gy@blurgy.xyz>
2020-11-25 11:25:47 +0800

    Add cam extrinsincs and matrix of perspective to orthographic projection
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- include/Zbuf.cpp
+++ include/Zbuf.cpp
@@ -5,15 +5,18 @@ Zbuf::Zbuf() { init(); }
 Zbuf::Zbuf(Scene const &s) : scene(s) { init(); }
 void Zbuf::init() { eye_initialized = false; }
 
-void Zbuf::set_cam(glm::vec3 const &ey, glm::vec3 const &g,
-                   glm::vec3 const &u) {
+void Zbuf::set_cam(glm::vec3 const &ey, flt const &_fov, flt const &ar,
+                   glm::vec3 const &g, glm::vec3 const &u) {
     eye             = ey;
     gaze            = g;
     up              = u;
+    fov             = _fov;
+    aspect_ratio    = ar;
     eye_initialized = true;
 }
 
-void Zbuf::init_mvp(glm::mat4 const &_model) {
+void Zbuf::init_mvp(flt const &near, flt const &far,
+                    glm::mat4 const &_model) {
     if (!eye_initialized) {
         errorm("Camera position is not initilized\n");
     }
@@ -23,7 +26,7 @@ void Zbuf::init_mvp(glm::mat4 const &_model) {
     /**/
     glm::vec3 right = glm::cross(gaze, up);
     // clang-format off
-    double view_value[] = {
+    flt view_value[] = {
         right.x, right.y, right.z, 0,
            up.x,    up.y,    up.z, 0,
         -gaze.x, -gaze.y, -gaze.z, 0,
@@ -34,7 +37,23 @@ void Zbuf::init_mvp(glm::mat4 const &_model) {
     view = glm::make_mat4(view_value);
 
     // Set projection matrix, according to fov, aspect ratio, etc.
-    // projection =
+    glm::vec4 persp_ortho; // Squeezes the frustum into a rectangular box
+    glm::vec4 ortho_trans; // Centers the rectangular box at origin
+    glm::vec4 ortho_scale; // Scales the rectangular box to the canonical cube
+    // clang-format off
+    flt po_value[] = { // values for persp_ortho
+        near,    0,          0,           0,
+           0, near,          0,           0,
+           0,    0, near + far, -near * far,
+           0,    0,          1,           0,
+    };
+    flt ot_value[] = { // values for ortho_trans
+
+    };
+    flt os_value[] = { // values for ortho_scale
+
+    };
+    // clang-format on
 }
 
 void Zbuf::naive() {}
--- include/Zbuf.hpp
+++ include/Zbuf.hpp
@@ -4,21 +4,24 @@
 #include "Scene.hpp"
 #include "global.hpp"
 
+#include <glm/ext/matrix_transform.hpp>
 #include <glm/glm.hpp>
 
 class Zbuf {
   private:
     Scene scene; // Scene to be rendered
 
-    glm::vec3 eye;  // Camera position
-    glm::vec3 gaze; // Camera's gaze direction
-    glm::vec3 up;   // Camera's top direction
+    glm::vec3 eye;          // Camera position
+    glm::vec3 gaze;         // Camera's gaze direction
+    glm::vec3 up;           // Camera's top direction
+    flt       fov;          // Field of view (Vertical, in degrees)
+    flt       aspect_ratio; // \frac{height}{width}
     bool      eye_initialized;
 
     glm::mat4 model; // Transform the model to the next timestep
     glm::mat4 view;  // Transform the camera to origin, model moves with it
     glm::mat4 projection; // M_{persp} @ M_{ortho}, where M_{ortho}
-                          // "compresses" the scene into the canonical box;
+                          // "compresses" the scene into the canonical cube;
                           // M_{persp} performs perspective projection.
 
     glm::mat4 mvp; // mvp = projection @ view @ model,
@@ -30,10 +33,17 @@ class Zbuf {
     // Set default values
     void init();
 
-    void set_cam(glm::vec3 const &ey, glm::vec3 const &g, glm::vec3 const &u);
+    void set_cam(glm::vec3 const &ey, flt const &_fov, flt const &ar,
+                 glm::vec3 const &g = glm::vec3(0, 0, -1),
+                 glm::vec3 const &u = glm::vec3(0, 1, 0));
 
     // Set mvp transformation matrix
-    void init_mvp(glm::mat4 const &_model = glm::mat4(0));
+    // @param   near: Near clipping plane's z coordinate
+    // @param    far: Far clipping plane's z coordinate
+    // @param _model: Model's transformation matrix, uses identity if not
+    // specified
+    void init_mvp(flt const &near, flt const &far,
+                  glm::mat4 const &_model = glm::identity<glm::mat4>());
 
     void naive(); // Naive z-buffer
 };
