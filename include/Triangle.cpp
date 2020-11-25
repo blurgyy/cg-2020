801548f6597bc867340815b0481e2c7b4e9ba748
Gaoyang Zhang <gy@blurgy.xyz>
2020-11-25 16:40:07 +0800

    Add face culling utility by computing a triangle's facing direction
    The order of the triangle's vertices should be counter-clockwise, as
    noted in class Triangle
    
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- include/Triangle.cpp
+++ include/Triangle.cpp
@@ -1,13 +1,21 @@
 #include "Triangle.hpp"
 
 Triangle::Triangle() {}
+Triangle::Triangle(std::array<glm::vec3, 3> const &vtx,
+                   std::array<glm::vec3, 3> const &nor,
+                   std::array<glm::vec3, 3> const &col)
+    : vtx{vtx[0], vtx[1], vtx[2]}, nor{nor[0], nor[1], nor[2]}, col{col[0],
+                                                                    col[1],
+                                                                    col[2]} {
+    // Initialize facing direction
+    facing = glm::cross(vtx[1] - vtx[0], vtx[2] - vtx[1]);
+}
 Triangle::Triangle(glm::vec3 const &a, glm::vec3 const &b, glm::vec3 const &c,
                    glm::vec3 const &na, glm::vec3 const &nb,
                    glm::vec3 const &nc, glm::vec3 const &ca,
                    glm::vec3 const &cb, glm::vec3 const &cc)
     : vtx{a, b, c}, nor{na, nb, nc}, col{ca, cb, cc} {
-    // vtx[0] = a, vtx[1] = b, vtx[2] = c;
-    // nor[0] = na, nor[1] = nb, nor[2] = nc;
+    facing = glm::cross(vtx[1] - vtx[0], vtx[2] - vtx[1]);
 }
 
 glm::vec3 Triangle::a() const { return vtx[0]; }
@@ -17,6 +25,7 @@ glm::vec3 Triangle::c() const { return vtx[2]; }
 glm::vec3 Triangle::na() const { return nor[0]; }
 glm::vec3 Triangle::nb() const { return nor[1]; }
 glm::vec3 Triangle::nc() const { return nor[2]; }
+
 
 // Author: Blurgy <gy@blurgy.xyz>
 // Date:   Nov 23 2020, 13:29 [CST]
--- include/Triangle.hpp
+++ include/Triangle.hpp
@@ -1,16 +1,23 @@
 #ifndef TRIANGLE_HPP
 #define TRIANGLE_HPP
 
+#include "global.hpp"
+
 #include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class Triangle {
-  private:
-    glm::vec3 vtx[3]; // Spatial coordinate of the 3 vertices
+  public:
+    glm::vec3 vtx[3]; // Spatial coordinate of the 3 vertices, in
+                      // counter-clockwise order
+    glm::vec3 facing; // Facing direction (for face culling), computed by:
+                      // glm::cross(v[1] - v[0], v[2] - v[1]);
     glm::vec3 nor[3]; // Normal directions of the 3 vertices
     glm::vec3 col[3]; // Color values of the 3 vertices
 
   public:
     Triangle();
+    // Assumes indices is {0, 1, 2}
     Triangle(glm::vec3 const &a, glm::vec3 const &b, glm::vec3 const &c,
              glm::vec3 const &na = glm::vec3(0),
              glm::vec3 const &nb = glm::vec3(0),
@@ -18,6 +25,18 @@ class Triangle {
              glm::vec3 const &ca = glm::vec3(0),
              glm::vec3 const &cb = glm::vec3(0),
              glm::vec3 const &cc = glm::vec3(0));
+    Triangle(std::array<glm::vec3, 3> const &vtx,
+             std::array<glm::vec3, 3> const &nor =
+                 {
+                     glm::vec3(0),
+                     glm::vec3(0),
+                     glm::vec3(0),
+                 },
+             std::array<glm::vec3, 3> const &col = {
+                 glm::vec3(0),
+                 glm::vec3(0),
+                 glm::vec3(0),
+             });
 
     // returns spatial location of the vertex
     glm::vec3 a() const;
@@ -28,6 +47,27 @@ class Triangle {
     glm::vec3 na() const;
     glm::vec3 nb() const;
     glm::vec3 nc() const;
+
+  public: // Operator overrides
+    friend Triangle operator*(glm::mat4 const &m, Triangle const &self) {
+        // Assign color values and indices of each vertex to the returned
+        // triangle.  Positions of vertices are overwritten, normal directons
+        // of vertices doesn't matter.
+        Triangle ret(self);
+
+        for (int i = 0; i < 3; ++i) {
+            auto const &v            = self.vtx[i];
+            flt         homo_value[] = {v.x, v.y, v.z, 1};
+            glm::vec4   homo         = glm::make_vec4(homo_value);
+
+            homo         = m * homo;
+            ret.vtx[i].x = homo.x / homo.w;
+            ret.vtx[i].y = homo.y / homo.w;
+            ret.vtx[i].z = homo.z / homo.w;
+        }
+
+        return ret;
+    }
 };
 
 #endif
