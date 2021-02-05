0341269f91d742dbc705f49f4fd74b37bd984a35
Gaoyang Zhang <gy@blurgy.xyz>
2021-02-05 12:07:37 +0800

    Fix zbuffer
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- pa1/include/Triangle.cpp
+++ pa1/include/Triangle.cpp
@@ -119,8 +119,9 @@ Triangle Triangle::operator*(mat4 const &m) const {
         ret.v[i].y = homo.y / homo.w;
         ret.v[i].z = homo.z / homo.w;
     }
-    // Note: Remember to update bbox and facing directions.
-    ret._init();
+    // !! Do not update facing direction when converting to view space.
+    // // Note: Remember to update bbox and facing directions.
+    // ret._init();
     return ret;
 }
 // Matrix right multiplication.
@@ -140,8 +141,9 @@ Triangle operator*(mat4 const &m, Triangle const &t) {
         ret.v[i].y = homo.y / homo.w;
         ret.v[i].z = homo.z / homo.w;
     }
-    // Note: Remember to update bbox and facing directions.
-    ret._init();
+    // !! Do not update facing direction when converting to view space.
+    // // Note: Remember to update bbox and facing directions.
+    // ret._init();
     return ret;
 }
 // Calculate barycentric coordinates of point 'pos' in triangle 't'
